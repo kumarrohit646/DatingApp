177a4d95d07145a1ae239aadbec1d82b77f51150
kumarrohit646 <kumarrohit646@gmail.com>
2024-03-14 23:59:47 +0530

    Added Web API Skeletor

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController
+{
+    private static readonly string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public WeatherForecastController()
+    {
+        
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        )).ToArray();
+    }
+
+
+}
+
